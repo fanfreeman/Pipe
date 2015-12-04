1be9354d3f195ec140a6c6edb934eb6a624f474f
Freeman Fan <fanfreeman@gmail.com>
2015-12-04 18:53:49 +0800

    first commit

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pipe : MonoBehaviour {
+
+    public float curveRadius, pipeRadius;
+
+    public int curveSegmentCount, pipeSegmentCount;
+
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private int[] triangles;
+
+    private void Awake()
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Pipe";
+        SetVertices();
+        SetTriangles();
+    }
+
+    private Vector3 GetPointOnTorus(float u, float v)
+    {
+        Vector3 p;
+        float r = (curveRadius + pipeRadius * Mathf.Cos(v));
+        p.x = r * Mathf.Sin(u);
+        p.y = r * Mathf.Cos(u);
+        p.z = pipeRadius * Mathf.Sin(v);
+        return p;
+    }
+
+    private void OnDrawGizmos()
+    {
+        float uStep = (2f * Mathf.PI) / curveSegmentCount;
+        float vStep = (2f * Mathf.PI) / pipeSegmentCount;
+
+        for (int u = 0; u < curveSegmentCount; u++)
+        {
+            for (int v = 0; v < pipeSegmentCount; v++)
+            {
+                Vector3 point = GetPointOnTorus(u * uStep, v * vStep);
+                Gizmos.color = new Color(
+                    1f,
+                    (float)v / pipeSegmentCount,
+                    (float)u / curveSegmentCount);
+                Gizmos.DrawSphere(point, 0.1f);
+            }
+        }
+    }
+
+    private void SetVertices()
+    {
+        vertices = new Vector3[pipeSegmentCount * curveSegmentCount * 4];
+        float uStep = (2f * Mathf.PI) / curveSegmentCount;
+        CreateFirstQuadRing(uStep);
+        mesh.vertices = vertices;
+    }
+
+    private void CreateFirstQuadRing(float u)
+    {
+        float vStep = (2f * Mathf.PI) / pipeSegmentCount;
+
+        Vector3 vertexA = GetPointOnTorus(0f, 0f);
+        Vector3 vertexB = GetPointOnTorus(u, 0f);
+        for (int v = 1, i = 0; v <= pipeSegmentCount; v++, i += 4)
+        {
+            vertices[i] = vertexA;
+            vertices[i + 1] = vertexA = GetPointOnTorus(0f, v * vStep);
+            vertices[i + 2] = vertexB;
+            vertices[i + 3] = vertexB = GetPointOnTorus(u, v * vStep);
+        }
+    }
+
+    private void SetTriangles()
+    {
+        triangles = new int[pipeSegmentCount * curveSegmentCount * 6];
+        for (int t = 0, i = 0; t < triangles.Length; t += 6, i += 4)
+        {
+            triangles[t] = i;
+            triangles[t + 1] = triangles[t + 4] = i + 1;
+            triangles[t + 2] = triangles[t + 3] = i + 2;
+            triangles[t + 5] = i + 3;
+        }
+        mesh.triangles = triangles;
+    }
+}
