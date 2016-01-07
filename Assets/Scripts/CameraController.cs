8eec81bc31c8c593b123c05b5a18b77b85bc9986
Freeman Fan <fanfreeman@gmail.com>
2016-01-07 17:43:35 +0800

    prevent staring at ground by moving and rotating camera at sharp pipe turns

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,8 +17,19 @@ public class CameraController : MonoBehaviour {
     private Pipe[] currentPipes = new Pipe[NumDataPoints];
 
     public GameObject vehicleAndCameraObject;
-
     public GameObject vehicleObject;
+    public GameObject cameraObject;
+
+    // min pos and rotation are the normal
+    // max pos and rotation are for preventing staring at the ground
+    private const float CameraMinY = 0.53f;
+    private const float CameraMaxY = 1f;
+    private const float CameraMinZ = -1f;
+    private const float CameraMaxZ = -1.7f;
+    private const float CameraMinRotationX = 0;
+    private const float CameraMaxRotationX = 20f; // degrees
+    private const float CameraDeltaY = 1f;
+    private const float CameraDeltaRotationX = -1f;
     
     void Start()
     {
@@ -99,6 +110,62 @@ public class CameraController : MonoBehaviour {
         Vector3 vehicleDirection = player.GetCenterTrackPointDirection();
         Quaternion vehicleRotation = Quaternion.LookRotation(vehicleDirection, player.GetUpVector());
         vehicleObject.transform.rotation = Quaternion.RotateTowards(vehicleObject.transform.rotation, vehicleRotation, Time.deltaTime * 300f);
+
+        // prevent staring at ground
+        int layerMask = 1 << 8; // only cast ray again the Pipe layer
+        Vector3 screenPosition = new Vector3(Screen.width / 2f, Screen.height / 2f, 0);
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 10f, layerMask))
+        {
+            Vector3 cameraLocalPosition = cameraObject.transform.localPosition;
+            // move y up
+            if (cameraLocalPosition.y < CameraMaxY)
+            {
+                cameraLocalPosition.y += Time.deltaTime * CameraDeltaY;
+                cameraObject.transform.localPosition = cameraLocalPosition;
+            }
+
+            // move z backwards
+            if (cameraLocalPosition.z > CameraMaxZ)
+            {
+                cameraLocalPosition.z -= Time.deltaTime * CameraDeltaY;
+                cameraObject.transform.localPosition = cameraLocalPosition;
+            }
+
+            // rotate around x-axis
+            Quaternion cameraLocalRotation = cameraObject.transform.localRotation;
+            if (cameraLocalRotation.eulerAngles.x < CameraMaxRotationX)
+            {
+                cameraObject.transform.Rotate(Time.deltaTime * CameraDeltaRotationX, 0, 0);
+            }
+
+            Debug.DrawLine(ray.origin, hit.point);
+            Debug.Log("hit ground");
+        } else
+        {
+            // move y back down
+            Vector3 cameraLocalPosition = cameraObject.transform.localPosition;
+            if (cameraLocalPosition.y > CameraMinY)
+            {
+                cameraLocalPosition.y -= Time.deltaTime * CameraDeltaY;
+                cameraObject.transform.localPosition = cameraLocalPosition;
+            }
+
+            // move z forward
+            if (cameraLocalPosition.z < CameraMinZ)
+            {
+                cameraLocalPosition.z += Time.deltaTime * CameraDeltaY;
+                cameraObject.transform.localPosition = cameraLocalPosition;
+            }
+
+            // rotate around x-axis
+            Quaternion cameraLocalRotation = cameraObject.transform.localRotation;
+            if (cameraLocalRotation.eulerAngles.x > CameraMinRotationX)
+            {
+                cameraObject.transform.Rotate(Time.deltaTime * -CameraDeltaRotationX, 0, 0);
+            }
+        }
     }
 
     private void OnDrawGizmos()
